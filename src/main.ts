0068eb8677bdd48c0f2ff2c1aad241d89f995064
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-02-16 00:41:38 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import 'zone.js/dist/zone';
 let title = "Tabata Timer";
 document.getElementById('app').innerHTML = title;
 
-let restartTimer = setInterval;
-
 let acao = '-';
 let ciclo = 0;
 let tempo = 5;
-let intervalId = setInterval(() => {
+let restartTimer = setInterval(() => {
   if (ciclo < 1) acao = 'Preparar!';
   else if (ciclo > 8 ) acao = "Sessão concluída com sucesso!";
   else if (tempo > 10) acao = "Comece o exercício!";
-});
+});;
