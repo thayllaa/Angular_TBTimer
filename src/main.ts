ae94914ad60e743f59697f14612822ec21b9e24f
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-02-16 01:07:33 -0300

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,4 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: ``,
-})
-export class App {
-  name = 'Temporizador Tabata ';
-}
-
-bootstrapApplication(App);
 
 let title = "Tabata Timer";
 document.getElementById('app').innerHTML = title;
