15a2860803470a45d4b86c955f716e431e25b2e2
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-02-16 01:59:46 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -29,4 +29,10 @@ let intervalId = setInterval(() => {
     ciclo += 1;
     tempo = 30;
   }
+
+  function startover() {
+    document.getElementById('restart-btn').addEventListener("click", () => {
+      startover();
+    });
+  }
 }, 1000);
