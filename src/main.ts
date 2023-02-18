4bba795cdf3afd54c66290077ca4fe01c94beadf
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-02-18 14:32:26 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,34 @@ let acao = '-';
 let ciclo = 0;
 let tempo = 5;
 
-let intervalId = setInterval(() => {
-  if (ciclo < 1) acao = 'Preparar!';
-  else if (ciclo > 8) acao = 'Sessão concluída com sucesso!';
-  //else if (ciclo > 8) acao = `restart-btn`;
-  else if (tempo > 10) acao = 'Comece o exercício!';
-  else acao = 'Você pode descansar (=';
-  document.getElementById('acao').innerHTML = acao;
+function restartTimer() {
+  let intervalId = setInterval(() => {
+    if (ciclo < 1) acao = 'Preparar!';
+    else if (ciclo > 8) acao = 'Sessão concluída com sucesso!';
+    //else if (ciclo > 8) acao = `restart-btn`;
+    else if (tempo > 10) acao = 'Comece o exercício!';
+    else acao = 'Você pode descansar (=';
+    document.getElementById('acao').innerHTML = acao;
 
-  if (ciclo >= 1 && ciclo <= 8) {
-    document.getElementById('ciclo').innerHTML = ciclo.toString();
-  } else {
-    document.getElementById('ciclo').innerHTML = '-';
-  }
-  if (ciclo <= 8) {
-    document.getElementById('tempo').innerHTML = tempo.toString();
-  } else {
-    document.getElementById('tempo').innerHTML = '-';
-  }
+    if (ciclo >= 1 && ciclo <= 8) {
+      document.getElementById('ciclo').innerHTML = ciclo.toString();
+    } else {
+      document.getElementById('ciclo').innerHTML = '-';
+    }
+    if (ciclo <= 8) {
+      document.getElementById('tempo').innerHTML = tempo.toString();
+    } else {
+      document.getElementById('tempo').innerHTML = '-';
+    }
 
-  tempo -= 1;
-  if (tempo == 0) {
-    ciclo += 1;
-    tempo = 30;
-  }
-}, 100);
+    document.getElementById(`restart-btn`).addEventListener(`click`, () => {
+      restartTimer();
+    });
+
+    tempo -= 1;
+    if (tempo == 0) {
+      ciclo += 1;
+      tempo = 30;
+    }
+  }, 100);
+}
